dcd738b298bcadd4d58132e86dd4b0f76eb4001c
Befous <befousmain@gmail.com>
2023-12-18 18:50:50 +0700

    Menggunakan crud sendiri

--- crud.go
+++ crud.go
@@ -3,7 +3,6 @@ package befous
 import (
 	"os"
 
-	"github.com/aiteung/atdb"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -11,11 +10,11 @@ import (
 // ------------------------------------------------------------------ Set Connection ------------------------------------------------------------------
 
 func SetConnection(mongoenv, dbname string) *mongo.Database {
-	var DBmongoinfo = atdb.DBInfo{
+	var DBmongoinfo = DBInfo{
 		DBString: os.Getenv(mongoenv),
 		DBName:   dbname,
 	}
-	return atdb.MongoConnect(DBmongoinfo)
+	return MongoConnect(DBmongoinfo)
 }
 
 func SetConnection2dsphere(mongoenv, dbname, collname string) *mongo.Database {
@@ -32,23 +31,23 @@ func SetConnection2dsphere(mongoenv, dbname, collname string) *mongo.Database {
 // Create
 
 func InsertUser(mongoenv *mongo.Database, collname string, datauser User) interface{} {
-	return atdb.InsertOneDoc(mongoenv, collname, datauser)
+	return InsertOneDoc(mongoenv, collname, datauser)
 }
 
 // Read
 
 func GetAllUser(mconn *mongo.Database, collname string) []User {
-	return atdb.GetAllDoc[[]User](mconn, collname)
+	return GetAllDoc[[]User](mconn, collname)
 }
 
 func FindUser(mconn *mongo.Database, collname string, userdata User) User {
 	filter := bson.M{"username": userdata.Username}
-	return atdb.GetOneDoc[User](mconn, collname, filter)
+	return GetOneDoc[User](mconn, collname, filter)
 }
 
 func IsPasswordValid(mconn *mongo.Database, collname string, userdata User) bool {
 	filter := bson.M{"username": userdata.Username}
-	res := atdb.GetOneDoc[User](mconn, collname, filter)
+	res := GetOneDoc[User](mconn, collname, filter)
 	hashChecker := CheckPasswordHash(userdata.Password, res.Password)
 	return hashChecker
 }
@@ -62,14 +61,14 @@ func UsernameExists(mconn *mongo.Database, collname string, userdata User) bool 
 
 func EditUser(mconn *mongo.Database, collname string, datauser User) interface{} {
 	filter := bson.M{"username": datauser.Username}
-	return atdb.ReplaceOneDoc(mconn, collname, filter, datauser)
+	return ReplaceOneDoc(mconn, collname, filter, datauser)
 }
 
 // Delete
 
 func DeleteUser(mconn *mongo.Database, collname string, userdata User) interface{} {
 	filter := bson.M{"username": userdata.Username}
-	return atdb.DeleteOneDoc(mconn, collname, filter)
+	return DeleteOneDoc(mconn, collname, filter)
 }
 
 // ---------------------------------------------------------------------- Geojson ----------------------------------------------------------------------
@@ -77,21 +76,21 @@ func DeleteUser(mconn *mongo.Database, collname string, userdata User) interface
 // Create
 
 func PostPoint(mconn *mongo.Database, collection string, pointdata GeoJsonPoint) interface{} {
-	return atdb.InsertOneDoc(mconn, collection, pointdata)
+	return InsertOneDoc(mconn, collection, pointdata)
 }
 
 func PostLinestring(mconn *mongo.Database, collection string, linestringdata GeoJsonLineString) interface{} {
-	return atdb.InsertOneDoc(mconn, collection, linestringdata)
+	return InsertOneDoc(mconn, collection, linestringdata)
 }
 
 func PostPolygon(mconn *mongo.Database, collection string, polygondata GeoJsonPolygon) interface{} {
-	return atdb.InsertOneDoc(mconn, collection, polygondata)
+	return InsertOneDoc(mconn, collection, polygondata)
 }
 
 // Read
 
 func GetAllBangunan(mconn *mongo.Database, collname string) []GeoJson {
-	return atdb.GetAllDoc[[]GeoJson](mconn, collname)
+	return GetAllDoc[[]GeoJson](mconn, collname)
 }
 
 // Update
@@ -100,7 +99,7 @@ func GetAllBangunan(mconn *mongo.Database, collname string) []GeoJson {
 
 func DeleteGeojson(mconn *mongo.Database, collname string, userdata User) interface{} {
 	filter := bson.M{"username": userdata.Username}
-	return atdb.DeleteOneDoc(mconn, collname, filter)
+	return DeleteOneDoc(mconn, collname, filter)
 }
 
 func GeoIntersects(mconn *mongo.Database, collname string, coordinates Point) string {
@@ -126,47 +125,47 @@ func Box(mconn *mongo.Database, collname string, coordinates Polyline) string {
 // -------------------------------------------------------------------- Pemrograman --------------------------------------------------------------------
 
 func GetAllKegiatan(mconn *mongo.Database, collname string) []Kegiatan {
-	kegiatan := atdb.GetAllDoc[[]Kegiatan](mconn, collname)
+	kegiatan := GetAllDoc[[]Kegiatan](mconn, collname)
 	return kegiatan
 }
 
 func GetAllJadwal(mconn *mongo.Database, collname string) []Jadwal {
-	lokasi := atdb.GetAllDoc[[]Jadwal](mconn, collname)
+	lokasi := GetAllDoc[[]Jadwal](mconn, collname)
 	return lokasi
 }
 
 func InsertMahasiswa(mongoenv *mongo.Database, collname string, datamahasiswa Mahasiswa) interface{} {
-	return atdb.InsertOneDoc(mongoenv, collname, datamahasiswa)
+	return InsertOneDoc(mongoenv, collname, datamahasiswa)
 }
 
 func GetAllMahasiswa(mconn *mongo.Database, collname string) []Mahasiswa {
-	mahasiswa := atdb.GetAllDoc[[]Mahasiswa](mconn, collname)
+	mahasiswa := GetAllDoc[[]Mahasiswa](mconn, collname)
 	return mahasiswa
 }
 
 func InsertDosen(mongoenv *mongo.Database, collname string, datadosen Dosen) interface{} {
-	return atdb.InsertOneDoc(mongoenv, collname, datadosen)
+	return InsertOneDoc(mongoenv, collname, datadosen)
 }
 
 func GetAllDosen(mconn *mongo.Database, collname string) []Dosen {
-	dosen := atdb.GetAllDoc[[]Dosen](mconn, collname)
+	dosen := GetAllDoc[[]Dosen](mconn, collname)
 	return dosen
 }
 
 func InsertRuangan(mongoenv *mongo.Database, collname string, dataruangan Ruangan) interface{} {
-	return atdb.InsertOneDoc(mongoenv, collname, dataruangan)
+	return InsertOneDoc(mongoenv, collname, dataruangan)
 }
 
 func GetAllRuangan(mconn *mongo.Database, collname string) []Ruangan {
-	ruangan := atdb.GetAllDoc[[]Ruangan](mconn, collname)
+	ruangan := GetAllDoc[[]Ruangan](mconn, collname)
 	return ruangan
 }
 
 func InsertMatakuliah(mongoenv *mongo.Database, collname string, datamatakuliah Matakuliah) interface{} {
-	return atdb.InsertOneDoc(mongoenv, collname, datamatakuliah)
+	return InsertOneDoc(mongoenv, collname, datamatakuliah)
 }
 
 func GetAllMatakuliah(mconn *mongo.Database, collname string) []Matakuliah {
-	matakuliah := atdb.GetAllDoc[[]Matakuliah](mconn, collname)
+	matakuliah := GetAllDoc[[]Matakuliah](mconn, collname)
 	return matakuliah
 }
